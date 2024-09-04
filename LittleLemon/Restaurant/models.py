c9cf5d2d815d8213abf88daebfbbf5ffe89753d2
M. R. Hossain <rez182@gmail.com>
2024-09-03 23:22:50 -0400

    added Menu model

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -5,3 +5,9 @@ class Booking(models.Model):
     name = models.CharField(max_length=255)
     no_of_guests = models.IntegerField()
     booking_date = models.DateTimeField()
+
+class Menu(models.Model):
+    id = models.IntegerField(primary_key=True)
+    title = models.CharField(max_length=255)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    inventory = models.IntegerField()
