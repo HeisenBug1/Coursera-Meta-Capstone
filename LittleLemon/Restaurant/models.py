0c90d8e14c59b95173ea179443a65317f0c520b0
M. R. Hossain <rez182@gmail.com>
2024-09-03 23:17:24 -0400

    Added Booking model

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Booking(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=255)
+    no_of_guests = models.IntegerField()
+    booking_date = models.DateTimeField()
