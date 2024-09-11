6e6234293d7931259727f17fcebfc5530650d150
M. R. Hossain <rez182@gmail.com>
2024-09-10 21:31:51 -0400

    included TIP from prompt: UserSerializer

--- LittleLemon/Restaurant/serializers.py
+++ LittleLemon/Restaurant/serializers.py
@@ -10,3 +10,12 @@ class BookingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Booking
         fields = ['id', 'name', 'no_of_guests', 'booking_date']
+
+
+# this UserSerializer class was included as TIP in the prompt (not sure when i'll use it)
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'groups']
