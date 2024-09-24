0be1eabd9d0b0bd839dce8364b76901c249bb5cb
M. R. Hossain <rez182@gmail.com>
2024-09-24 15:13:24 -0400

    Update Int to Auto for ID
    Tests fail if ID is not provided during creation testing.

--- LittleLemon/Restaurant/models.py
+++ LittleLemon/Restaurant/models.py
@@ -10,7 +10,7 @@ class Booking(models.Model):
         return f"{self.name} - {self.booking_date}"
 
 class Menu(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     inventory = models.IntegerField()
